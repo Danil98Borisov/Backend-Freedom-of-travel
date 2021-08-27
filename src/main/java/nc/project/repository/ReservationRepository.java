defb83449a5f2b4ac160418db4f47373af4766d5
Danil98Borisov <danil1998borisov@gmail.com>
2021-08-27 12:40:36 +0300

    Adding Repository

---
+++ src/main/java/nc/project/repository/ReservationRepository.java
@@ -0,0 +1,18 @@
+package nc.project.repository;
+
+import nc.project.models.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long>{
+
+}
+
+
+
+
