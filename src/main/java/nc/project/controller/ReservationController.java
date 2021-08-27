762b4ff97de25bfa4633023296941656b6e4dc16
Danil98Borisov <danil1998borisov@gmail.com>
2021-08-27 15:38:36 +0300

    Correction by comments

--- src/main/java/nc/project/controller/ReservationController.java
+++ src/main/java/nc/project/controller/ReservationController.java
@@ -1,9 +1,23 @@
 package nc.project.controller;
 
+import nc.project.models.Reservation;
+import nc.project.repository.ReservationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-
 
 @RestController
 public class ReservationController {
+    @Autowired
+    private ReservationRepository reservationRepository;
+
+    @GetMapping("/reservation")
+    public Iterable<Reservation> findAll() {
+
+        return reservationRepository.findAll();
+    }
+
+    @DeleteMapping("/reservation/delete/{id}")
+    void deleteReservation(@PathVariable Long id) {
+        reservationRepository.deleteById(id);
+    }
 }
