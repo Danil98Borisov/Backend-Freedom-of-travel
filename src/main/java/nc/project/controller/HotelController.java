9ddf32145a7a996e382d8c848ca4ecbcc0f783f3
Danil98Borisov <danil1998borisov@gmail.com>
2021-08-27 13:39:13 +0300

    Adding Controller

---
+++ src/main/java/nc/project/controller/HotelController.java
@@ -0,0 +1,24 @@
+package nc.project.controller;
+
+import nc.project.models.Hotel;
+import nc.project.repository.HotelRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+public class HotelController {
+    @Autowired
+    private HotelRepository hotelRepository;
+
+
+    @GetMapping("/hotels/filter")
+    List<Hotel> dateHotel(@RequestParam (required = false, defaultValue = "0") int rating){
+
+        return hotelRepository.findHotelByRating(rating);
+    }
+
+}
+
