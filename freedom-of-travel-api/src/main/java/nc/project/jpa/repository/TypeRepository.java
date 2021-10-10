7b6d23f58fc4e875f1dd0f9dd123db485fc4fdd9
Danil98Borisov <danil1998borisov@gmail.com>
2021-10-10 13:34:27 +0300

    Rebasing on master

--- freedom-of-travel-api/src/main/java/nc/project/jpa/repository/TypeRepository.java
+++
@@ -1,9 +0,0 @@
-package nc.project.jpa.repository;
-
-import nc.project.jpa.entity.ApartmentType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TypeRepository extends JpaRepository<ApartmentType, Long> {
-}
