a3fb9d324f141fa6b7cae78be0be492bc2260383
Danil98Borisov <87095649+Danil98Borisov@users.noreply.github.com>
2021-10-12 23:06:04 +0300

    Message about booking an apartment. (#69)
    [UI] Email notification about successful reservation

---
+++ freedom-of-travel-api/src/main/java/nc/project/auth/response/EmailReservationResponse.java
@@ -0,0 +1,14 @@
+package nc.project.auth.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmailReservationResponse {
+
+    private String message;
+
+}
